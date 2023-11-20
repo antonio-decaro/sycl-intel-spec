3cadc297dd314928de956cbe0b99298efaab2f5d
antonio-decaro <antonio.decaro99@outlook.it>
2023-11-20 19:53:43 +0100

    Update parameters for scalar_prod

--- run.sh
+++ run.sh
@@ -34,7 +34,7 @@ $SCRIPT_DIR/build/nbody \
   --size=4096 --num-iters=1 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/NBody.log
 echo "scalar_prod"
 $SCRIPT_DIR/build/scalar_prod \
-  --size=4194304 --num-iters=100000 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/ScalarProd.log
+  --size=2097152 --num-iters=100000 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/ScalarProd.log
 echo "sobel"
 $SCRIPT_DIR/build/sobel \
   --size=1536 --num-iters=1 --device=gpu --no-verification --num-runs=$runs > $SCRIPT_DIR/logs/Sobel.log
