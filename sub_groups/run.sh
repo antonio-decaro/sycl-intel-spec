6447e371ef9e167970c9cac14896345e2d1c6263
antonio-decaro <antonio.decaro99@outlook.it>
2023-12-19 18:54:04 +0100

    Update benchmark iterations

--- sub_groups/run.sh
+++ sub_groups/run.sh
@@ -26,34 +26,34 @@ mkdir -p $SCRIPT_DIR/logs
 
 echo "vec_add"
 $BENCH_DIR/vec_add \
-  --size=1000000 --num-iters=100000 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/VectorAddition.log
+  --size=1000000 --num-iters=1 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/VectorAddition.log
 echo "matrix_mul"
 $BENCH_DIR/matrix_mul \
-  --size=8192 --num-iters=5 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/MatrixMul.log
+  --size=4096 --num-iters=1 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/MatrixMul.log
 echo "nbody"
 $BENCH_DIR/nbody \
   --size=8192 --num-iters=1 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/NBody.log
 echo "scalar_prod"
 $BENCH_DIR/scalar_prod \
-  --size=8388608 --num-iters=100000 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/ScalarProd.log
+  --size=8388608 --num-iters=1 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/ScalarProd.log
 echo "sobel"
 $BENCH_DIR/sobel \
   --size=4096 --num-iters=1 --device=gpu --no-verification --num-runs=$runs > $SCRIPT_DIR/logs/Sobel.log
 echo "median"
 $BENCH_DIR/median \
-  --size=4096 --num-iters=1000 --device=gpu --no-verification --num-runs=$runs > $SCRIPT_DIR/logs/Median.log
+  --size=4096 --num-iters=1 --device=gpu --no-verification --num-runs=$runs > $SCRIPT_DIR/logs/Median.log
 echo "lin_reg_coeff"
 $BENCH_DIR/lin_reg_coeff \
-  --size=8388608 --num-iters=1000 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/LinRegCoeff.log
+  --size=8388608 --num-iters=1 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/LinRegCoeff.log
 echo "kmeans"
 $BENCH_DIR/kmeans \
-  --size=16777216 --num-iters=50000 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/KMeans.log
+  --size=16777216 --num-iters=1 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/KMeans.log
 echo "mol_dyn"
 $BENCH_DIR/mol_dyn \
-  --size=16777216 --num-iters=200000 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/MolDyn.log
+  --size=16777216 --num-iters=1 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/MolDyn.log
 echo "merse_twister"
 $BENCH_DIR/merse_twister \
-  --size=33554432 --num-iters=50000 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/MerseTwister.log
+  --size=33554432 --num-iters=1 --device=gpu --num-runs=$runs > $SCRIPT_DIR/logs/MerseTwister.log
 
 
 echo "[*] Done"
